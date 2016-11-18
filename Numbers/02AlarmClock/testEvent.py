1ae0c99ab0cb5befe03af1633b641f6683f73a5f
zwdnet <zwdnet@163.com>
2016-11-18 19:30:18 +0800

    测试定时器的用法

--- Numbers/02AlarmClock/testEvent.py
+++ Numbers/02AlarmClock/testEvent.py
@@ -1,20 +1,87 @@
 # -*- coding:utf-8 -*-
+# from threading import Timer
+# import time
+#
+# timer_interval = 1
+# j = 0
+#
+#
+# def delayrun():
+#     global j
+#     j = j + 1
+#     print("running", j)
+#
+# t = Timer(timer_interval, delayrun)
+# t.start()
+# i = 0
+# while True:
+#     time.sleep(0.1)
+#     i = i + 1
+#     print("main running", i)
+
+# import time, gevent
+#
+#
+# def print_time(sleep_time, tag):
+#     print(time.time(), tag)
+#     time.sleep(sleep_time)
+#
+#
+# def run_regularly(function, intervals, sleep_time=0.1, round_length=1):
+#     _, init = divmod(time.time(), 1)
+#     gevent.sleep(1 - init)
+#     while True:
+#         before = time.time()
+#         _, offset = divmod(before, round_length)
+#         for div in intervals:
+#             function(sleep_time, div)
+#             after = time.time() - before
+#             if after < (div * round_length):
+#                 gevent.sleep((div * round_length) - after - (offset/len(intervals)))
+#
+# gevent.spawn(run_regularly(), print_time, [0.2, 0.8, 1.0])
+#
+# while True:
+#     gevent.sleep(0.1)
 from threading import Timer
-import time
 
-timer_interval = 1
-j = 0
+class MyTimer:
+
+    def __init__(self):
+        self._timer = None
+        self._tm = None
+        self._fn = None
+
+    def _do_func(self):
+        if self._fn:
+            self._fn()
+            self._do_start()
+
+    def _do_start(self):
+        self._timer = Timer(self._tm, self._do_func)
+        self._timer.start()
+
+    def start(self, tm, fn):
+        self._fn = fn
+        self._tm = tm
+        self._do_start()
+
+    def stop(self):
+        try:
+            self._timer.cancel()
+        except:
+            pass
 
 
-def delayrun():
-    global j
-    j = j + 1
-    print("running", j)
+def hello():
+    from datetime import datetime
+    print("hello world", datetime.now())
 
-t = Timer(timer_interval, delayrun)
-t.start()
-i = 0
-while True:
-    time.sleep(0.1)
-    i = i + 1
-    print("main running", i)
+
+if __name__ == "__main__":
+    mt = MyTimer()
+    mt.start(2, hello)
+    for i in range(10):
+        import time
+        time.sleep(1)
+    mt.stop()
